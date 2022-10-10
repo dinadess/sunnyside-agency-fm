59ba6548a47476287f7469a18d8a23732c12ce32
Orlandina DESSOUASSI <orlandina.dessouassi@askbenin.bj>
2022-10-10 20:30:08 +0200

    Solved accessibility issues

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const menu = document.querySelector('.menu');
-const navigation = document.querySelector('.navigation');
+"use strict";
 
-menu.addEventListener('click', function() {
-    navigation.classList.toggle('active');
-})
+// Navigation On Mobile Screens
+
+const menu = document.querySelector(".menu");
+const navigation = document.querySelector(".navigation");
+
+menu.addEventListener("click", function () {
+  navigation.classList.toggle("active");
+});
+
+// Scroll Into First Section When clicking Arrow
+
+const firstSectionEl = document.querySelector(".first-section");
+
+const arrowDown = document.querySelector(".arrow-down");
+
+arrowDown.addEventListener("click", function () {
+  firstSectionEl.scrollIntoView({ behavior: "smooth" });
+});
